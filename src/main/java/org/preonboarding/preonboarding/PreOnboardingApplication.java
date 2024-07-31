a3914ab7877c7e422856ef895786620c7221bd9f
Junyong <jypark93@kookmin.ac.kr>
2024-08-01 00:45:44 +0900

    Feature [#1] : 스프링 초기 설정
    - 스프링 초기 설정 완료

---
+++ src/main/java/org/preonboarding/preonboarding/PreOnboardingApplication.java
@@ -0,0 +1,13 @@
+package org.preonboarding.preonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreOnboardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/preonboarding/preonboarding/PreOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.preonboarding.preonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
