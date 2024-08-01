14eef1651bc67155c1c6244b17b0414be8ab7bb1
Junyong <jypark93@kookmin.ac.kr>
2024-08-01 22:37:46 +0900

    Feature [#3] : 로그인 기능
    - 스프링 시큐리티를 이용한 로그인 기능 구현

---
+++ src/main/java/org/preonboarding/preonboarding/service/MyUserService.java
@@ -0,0 +1,7 @@
+package org.preonboarding.preonboarding.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserService {
+}
---
+++ src/test/java/org/preonboarding/preonboarding/service/MyUserServiceTest.java
@@ -0,0 +1,7 @@
+package org.preonboarding.preonboarding.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyUserServiceTest {
+
+}
